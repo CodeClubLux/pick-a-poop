c48544a35caffbef94489072c4520c930bcbdeea
linnea <linneapup10@gmail.com>
2016-03-31 20:41:10 +0200

    added a html doc called responsive.html and a java script doc called events.js

---
+++ web/Linnea/events.js
@@ -0,0 +1,10 @@
+$('body').click(function(e) {
+   //alert(e.target.id);
+   $("#OL_Icon_58_innerImage").attr("src","Dog_logo_computer_beta.png");
+});
+
+ 
+$(document).on("click",function(e) {
+  //  alert(e.target.id);
+});
+
